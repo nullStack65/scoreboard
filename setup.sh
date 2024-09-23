b7d23f710a877cb2dd774e7bd6f3ae8bf7adec8d
Your Name <you@example.com>
2024-09-23 15:20:09 -0400

    v1.0.0

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ sudo apt install xorg python3-tk python3-pip
 pip3 install -r requirements.txt
 mv xinitrc ~/.xinitrc
 chmod +x ~/.xinitrc
+mv xorg.conf /etc/X11/xorg.conf
 echo "startx" >> ~/.bashrc
-#https://linuxgenie.net/setup-automatic-login-ubuntu-22-04/#post-13988-_ly3worrjd341
