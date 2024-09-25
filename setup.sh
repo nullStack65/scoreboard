da0e0b4e30eb8e029931d7bf3a196d4629beffdc
root <root@ASROCK-Desktop>
2024-09-25 19:59:28 -0400

    v2.0.1

--- setup.sh
+++ setup.sh
@@ -1,7 +1,26 @@
+#!/bin/bash
+
+# Update package lists
 sudo apt update
-sudo apt install xorg python3-tk python3-pip
+
+# Install Xorg, Python3 Tkinter, and pip (with automatic yes to prompts)
+sudo apt install xorg python3-tk python3-pip -y
+
+# Install Python requirements
 pip3 install -r requirements.txt
+
+# Move and set up .xinitrc
 mv xinitrc ~/.xinitrc
 chmod +x ~/.xinitrc
-mv xorg.conf /etc/X11/xorg.conf
-echo "startx" >> ~/.bashrc
+
+# Set up X11 configuration
+sudo mv xorg.conf /etc/X11/xorg.conf
+
+# Set up systemd service for starting X
+sudo mv setup.sh /etc/systemd/system/startx.service
+sudo systemctl daemon-reload
+sudo systemctl enable startx.service
+sudo systemctl start startx.service
+
+# Reboot the system
+sudo reboot
